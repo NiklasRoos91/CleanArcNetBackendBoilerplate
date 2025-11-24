27ff42ad4d4e6aa69c60d18b0b20e1104c53a6fa
NiklasRoos91 <niklas_roos@live.se>
2025-11-24 17:14:28 +0100

    Add Email value object to Domain

--- CleanArcNetBackendBoilerplate.Domain/Entities/User.cs
+++ CleanArcNetBackendBoilerplate.Domain/Entities/User.cs
@@ -1,12 +1,13 @@
-﻿namespace CleanArcNetBackendBoilerplate.Domain.Entities
+﻿using CleanArcNetBackendBoilerplate.Domain.ValueObjects;
+
+namespace CleanArcNetBackendBoilerplate.Domain.Entities
 {
     public class User
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public Email Email { get; set; }
 
         public string PasswordHash { get; private set; } = string.Empty;
-
     }
 }
---
+++ CleanArcNetBackendBoilerplate.Domain/ValueObjects/Email.cs
@@ -0,0 +1,24 @@
+﻿namespace CleanArcNetBackendBoilerplate.Domain.ValueObjects
+{
+    public class Email
+    {
+        public string Value { get; }
+
+        public Email(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value) || !value.Contains("@"))
+                throw new ArgumentException("Invalid email", nameof(value));
+
+            Value = value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Email other && Value == other.Value;
+        }
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public override string ToString() => Value;
+    }
+}
